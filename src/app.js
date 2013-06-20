ca7d02e530a36d5d045ab335072e27ba070c82f3
Dominick Pham <dominick.pham@gmail.com>
2013-06-20 01:15:01 +0000

    create setup block with a callback

--- src/app.js
+++ src/app.js
@@ -1,77 +1,70 @@
-var gpio = require('gpio'),
-	async = require('async');
+var gpio = require('gpio'), async = require('async');
 
+var
+SPICLK  = 18,
+SPIMISO = 23,
+SPIMOSI = 24,
+SPICS   = 25;
+
+var pins = {}, cspin, clockpin, mosipin, misopin;
+
+var setup = function(callback) {
+	mosipin = pins[SPIMOSI] = gpio.export(SPIMOSI, { direction: 'out' });
+	misopin = pins[SPIMISO] = gpio.export(SPIMISO, { direction: 'in' });
+	clockpin = pins[SPICLK] = gpio.export(SPICLK, { direction: 'out' });
+	cspin = pins[SPICS] = gpio.export(SPICS, {
+		direction: 'out',
+		ready: function() {
+			if(typeof callback === 'function') callback();
+		}
+	});
+};
 
 /**
  * Reads digital values from a MCP3004 ADC.
  * @param {Number} channel - the ADC channel to read
- * @param {Object} opts - an object containing the pin numbers
- *   for clock, mosi, miso and cspin
  * @return {Number} the read value between 0 and 1023
  */
-var readAdc = function(channel, callback, opts) {
+var readAdc = function(channel, callback) {
+	var cmdOut, adcOut;
 	if (channel < 0 || channel > 3) throw new Error('adc channel number must be in the range of 0--3');
-	// pin numbers from opts or default ones
-	var clockPin = opts && opts.clockPin ? opts.clockPin : 18,
-		misoPin = opts && opts.misoPin ? opts.misoPin : 23,
-		mosiPin = opts && opts.mosiPin ? opts.mosiPin : 24,
-		csPin = opts && opts.csPin ? opts.csPin : 25;
-	// direction of each pin
-	var pinsConf = [
-		{pin: clockPin, direction: 'out'},
-		{pin: misoPin, direction: 'in'},
-		{pin: mosiPin, direction: 'out'},
-		{pin: csPin, direction: 'out'}
-	];
-	// handle refs to the gpio instances
-	var pins = {};
 
-	var _initGpio = function(pinConf, done) {
-		pins[pinConf.pin] = gpio.export(pinConf.pin, {
-			direction: pinConf.direction,
-			ready: function() {
-				done();
-			}
-		});
-	};
-	// start reading spi data when all pins are ready
-	async.each(pinsConf, _initGpio, function(err) {
-		if (err) throw err;
-		pins[csPin].set(1);
-		pins[clockPin].set(0);
-		pins[csPin].set(0);
+	cspin.set();
+	clockpin.set(0);
+	cspin.set(0);
 
-		var cmdOut = channel;
-		cmdOut |= 0x18;
-		cmdOut <<= 3;
-		for (var i = 0; i < 5; i++) {
-			if (cmdOut & 0x80) {
-				pins[mosiPin].set(1);
-			}
-			else {
-				pins[mosiPin].set(0);
-			}
-			cmdOut <<= 1;
-			pins[clockPin].set(1);
-			pins[clockPin].set(0);
+	cmdOut = channel | 0x18;
+	cmdOut <<= 3;
+
+	for (var i = 0; i < 5; i++) {
+		if (cmdOut & 0x80) {
+			mosipin.set(1);
+		} else {
+			mosipin.set(0);
 		}
-		var adcOut = 0;
-		for (i = 0; i < 12; i++) {
-			pins[clockPin].set(1);
-			pins[clockPin].set(0);
-			adcOut <<= 1;
-			if (pins[misoPin].value) {
-				adcOut |= 0x1;
-			}
+		cmdOut <<= 1;
+		clockpin.set(1);
+		clockpin.set(0);
+	}
+
+	adcOut = 0;
+	for (i = 0; i < 12; i++) {
+		clockpin.set(1);
+		clockpin.set(0);
+		adcOut <<= 1;
+		if (misopin.value) {
+			adcOut |= 0x1;
 		}
-		pins[csPin].set(1);
-		adcOut >>= 1;
-		callback(adcOut);
-	});
+	}
+	cspin.set(1);
+	adcOut >>= 1;
+	callback(adcOut);
 };
 
-setInterval(function() {
-	readAdc(0, function(value) {
-		console.log(value);
-	});
-}, 1000);
+setup(function() {
+	setInterval(function() {
+		readAdc(0, function(value) {
+			console.log(value);
+		});
+	}, 500);
+});
